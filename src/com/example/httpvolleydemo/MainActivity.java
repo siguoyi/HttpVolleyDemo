7d89740e8ec5e0a32f48cf4a81694ec90170864a
antcl <believemine@163.com>
2016-03-03 17:15:40 +0800

    change post to download image use imageRequest and imadeLoader

--- src/com/example/httpvolleydemo/MainActivity.java
+++ src/com/example/httpvolleydemo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.httpvolleydemo;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.json.JSONObject;
@@ -9,17 +8,26 @@ import com.android.volley.Request.Method;
 import com.android.volley.AuthFailureError;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageLoader.ImageCache;
+import com.android.volley.toolbox.ImageLoader.ImageListener;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,7 +38,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	private Button bt_post;
 	private Button bt_json;
 	private TextView tv_get;
-	private TextView tv_post;
+	private ImageView imageview;
 	private TextView tv_json;
 	private RequestQueue requestQueue;
 	@Override
@@ -39,14 +47,14 @@ public class MainActivity extends Activity implements OnClickListener{
 		setContentView(R.layout.activity_main);
 		
 		bt_get = (Button) findViewById(R.id.bt_get);
-		bt_post = (Button) findViewById(R.id.bt_post);
+		bt_post = (Button) findViewById(R.id.bt_image);
 		bt_json = (Button) findViewById(R.id.bt_json);
 		bt_get.setOnClickListener(this);
 		bt_post.setOnClickListener(this);
 		bt_json.setOnClickListener(this);
 		
 		tv_get = (TextView) findViewById(R.id.tv_get);
-		tv_post = (TextView) findViewById(R.id.tv_post);
+		imageview = (ImageView) findViewById(R.id.imageview);
 		tv_json = (TextView) findViewById(R.id.tv_json);
 		requestQueue = Volley.newRequestQueue(this);
 	}
@@ -55,7 +63,7 @@ public class MainActivity extends Activity implements OnClickListener{
 		Log.d(TAG, "onClick");
 		switch (v.getId()) {
 		case R.id.bt_get:
-			Toast.makeText(MainActivity.this, "GET", Toast.LENGTH_SHORT).show();
+//			Toast.makeText(MainActivity.this, "GET", Toast.LENGTH_SHORT).show();
 			StringRequest getRequest = new StringRequest("http://www.baidu.com", new Response.Listener<String>() {
 
 				@Override
@@ -75,29 +83,41 @@ public class MainActivity extends Activity implements OnClickListener{
 			requestQueue.add(getRequest);
 			
 			break;
-		case R.id.bt_post:
-			StringRequest postRequest = new StringRequest(Method.POST, "http://www.baidu.com", new Response.Listener<String>() {
-
-				@Override
-				public void onResponse(String response) {
-					tv_post.setText(response);
-				}
-			}, new Response.ErrorListener() {
-
-				@Override
-				public void onErrorResponse(VolleyError error) {
-					tv_post.setText(error.getMessage());
-				}}){
-				@Override
-				protected Map<String, String> getParams() throws AuthFailureError {
-					Map<String, String> map = new HashMap<String, String>();
-					map.put("params1", "value1");
-					map.put("params2", "value2");
-					return map;
-				}
-			};
+		case R.id.bt_image:
+//			@SuppressWarnings("deprecation")
+//			ImageRequest imageRequest = new ImageRequest("http://buptant.cn/images/logo.jpg", new Listener<Bitmap>() {
+//
+//				@Override
+//				public void onResponse(Bitmap response) {
+//					imageview.setImageBitmap(response);
+//					
+//				}
+//			}, 0, 0, Config.RGB_565, new ErrorListener() {
+//
+//				@Override
+//				public void onErrorResponse(VolleyError error) {
+//					imageview.setImageResource(R.drawable.no_media);
+//				}
+//			});
+//			requestQueue.add(imageRequest);
 			
-			requestQueue.add(postRequest);
+			ImageLoader imageLoader = new ImageLoader(requestQueue, new ImageCache() {
+				
+				@Override
+				public void putBitmap(String url, Bitmap bitmap) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public Bitmap getBitmap(String url) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+			});
+			
+			ImageListener listener = ImageLoader.getImageListener(imageview, R.drawable.no_media, R.drawable.ic_launcher);
+			imageLoader.get("http://img.my.csdn.net/uploads/201404/13/1397393290_5765.jpeg", listener,300,300);
 			break;
 		case R.id.bt_json:
 			JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("http://m.weather.com.cn/data/101010100.html", null, 
